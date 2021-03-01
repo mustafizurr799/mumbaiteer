798380573160be3c0abf72e3f8657967f0b67db7
mustafizurr799 <67517921+mustafizurr799@users.noreply.github.com>
2021-03-01 22:11:03 +0530

    Add files via upload

---
+++ Assets/js/TimerScript.js
@@ -0,0 +1,121 @@
+﻿$(document).ready(function () {
+    timefunction();
+    setInterval(resultcountfunction, 5000);
+    setInterval(timefunction, 5000);
+    setInterval("timecountdown()", 1000);
+});
+var timetable = "";
+function resultcountfunction() {
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "PlayRoom/AjaxHandler.aspx/LastResultCounter",
+        dataType: "json",
+        success: function (data) {
+            var ncount = data.d;
+            var lcount = document.getElementById('hd_rocounter').value;
+            if (ncount != lcount) {
+                location.reload();
+            }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            // alert(errorThrown);
+        }
+    });
+}
+function timefunction() {
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "PlayRoom/AjaxHandler.aspx/GetServerTime",
+        dataType: "json",
+        success: function (data) {
+            timetable = data.d;
+            var arr = timetable.split('_');
+            if (arr.length == 5) {
+            }
+            else {
+                arr[0] = "00:00:00"; arr[1] = "00:00:00"; arr[2] = "00:00:00"; arr[3] = "dd:MM:YYYY"; arr[4] = "00:00:00";
+            }
+            document.getElementById('NextDrowTime').innerHTML = arr[0];
+            document.getElementById('LastDrawTime').innerHTML = arr[1];
+            document.getElementById('RemainTime').innerHTML = arr[2];
+            document.getElementById('NowTime').innerHTML = arr[3];
+            document.getElementById('TodatyDate').innerHTML = arr[4];
+            var ltime = document.getElementById('hd_nextime').value;
+            if (arr[0] != ltime) {
+                location.reload();
+            }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            // alert(errorThrown);
+        }
+    });
+}
+function timecountdown() {
+    var time = document.getElementById('RemainTime').innerHTML;
+    if (time != "Time Out") {
+        var timearr = time.split(':')
+        var h = timearr[0].trim();
+        var m = timearr[1].trim();
+        var s = timearr[2].trim();
+        if (parseInt(s) > 0) {
+            s = parseInt(s) - 1;
+
+        }
+        else {
+            if (parseInt(m) > 0) {
+
+                s = 59;
+                m = parseInt(m) - 1;
+            }
+            else {
+                if (parseInt(h) > 0) {
+                    s = 59;
+                    m = 59;
+                    h = parseInt(h) - 1;
+                }
+                else {
+                    window.location.reload();
+                }
+            }
+        }
+        if (h.toString().length < 2)
+            h = "0" + h;
+        if (m.toString().length < 2)
+            m = "0" + m;
+        if (s.toString().length < 2)
+            s = "0" + s;
+        document.getElementById('RemainTime').innerHTML = h + ":" + m + ":" + s;
+    }
+    var time1 = document.getElementById('NowTime').innerHTML;
+    var timearr1 = time1.split(':')
+    var h1 = timearr1[0].trim();
+    var m1 = timearr1[1].trim();
+    var s1 = timearr1[2].trim();
+    var tsarr = s1.split(' ')
+    s1 = tsarr[0].trim();
+    var ts = tsarr[1].trim();
+    if (parseInt(s1) < 60) {
+        s1 = parseInt(s1) + 1;
+        // alert(s);
+    }
+    else {
+        if (parseInt(m1) < 59) {
+            s1 = 0;
+            m1 = parseInt(m1) + 1;
+        }
+        else {
+            s1 = 0;
+            m1 = 0;
+            h1 = parseInt(h1) + 1;
+        }
+    }
+    if (h1.toString().length < 2)
+        h1 = "0" + h1;
+    if (m1.toString().length < 2)
+        m1 = "0" + m1;
+    if (s1.toString().length < 2)
+        s1 = "0" + s1;
+    document.getElementById('NowTime').innerHTML = h1 + ":" + m1 + ":" + s1 + " " + ts;
+}
---
+++ Assets/js/default.js
@@ -0,0 +1,113 @@
+﻿function OpenInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+}
+
+function digitArrowkey(c, e) {
+    var keyCode = (e.which) ? e.which : e.keyCode;
+    var x = 0;
+    var y = 0;
+    var BoxArray = new Array(60);
+    var i = 1;
+    for (i = 0; i < 60; i++) {
+        BoxArray[i] = "t_" + i;
+    }
+    if (keyCode == 37) {
+        c = parseInt(c) - 1;
+        if (c >= 0)
+            document.getElementById(BoxArray[c]).focus();
+        else
+            document.getElementById(BoxArray[29]).focus();
+    }
+    if (keyCode == 38) {
+        if (c <= 59)
+            c = parseInt(c) - 10;
+        if (c >= 0)
+            document.getElementById(BoxArray[c]).focus();
+    }
+    //right
+    if (keyCode == 39) {
+        c = parseInt(c) + 1;
+        if (c <= 59)
+            document.getElementById(BoxArray[c]).focus();
+        if (c == 60)
+            document.getElementById(BoxArray[0]).focus();
+    }
+    if (keyCode == 40) {
+        c = parseInt(c) + 10;
+        if (c <= 59)
+            document.getElementById(BoxArray[c]).focus();
+        if (c == 60)
+            document.getElementById(BoxArray[0]).focus();
+    }
+}
+function digitcalc() {
+
+    var x1 = 0;
+    var y1 = 0;
+    var TotalArray = new Array(6);
+    var index = 0;
+    var counter = 0;
+    var value = 0;
+    for (var j = 0; j <= 59; j++) {
+        value = document.getElementById('t_' + j).value;
+        var temp1 = parseInt(value);
+        if (!isNaN(temp1)) {
+            x1 = value;
+            y1 = parseInt(y1) + parseInt(x1);
+        }
+        else
+            document.getElementById('t_' + j).value = "";
+        index++;
+        if (index == 10) {
+            TotalArray[counter] = y1;
+            counter++;
+            x1 = 0;
+            y1 = 0;
+            index = 0;
+        }
+    }
+    var tjqty = 0;
+    var tjrs = 0;
+    var flage = false;
+
+    var tdqty = 0;
+    var tdrs = 0;
+    for (var i = 0; i < 6; i++) {
+        var temp1 = parseInt(TotalArray[i]);
+        if (!isNaN(temp1)) {
+            document.getElementById('lbl_qty' + i).innerHTML = TotalArray[i];
+            document.getElementById('lbl_amt' + i).innerHTML = TotalArray[i] * document.getElementById('hd_tr').value;
+            tdqty = tdqty + TotalArray[i];
+            tdrs = tdrs + (TotalArray[i] * document.getElementById('hd_tr').value);
+        }
+    }
+    var temp2 = parseInt(tdqty);
+    if (!isNaN(temp2)) {
+        document.getElementById('lbl_qty').innerHTML = tdqty;
+        document.getElementById('lbl_amt').innerHTML = tdrs;
+    }
+}
+function isNumber(evt) {
+    var iKeyCode = (evt.which) ? evt.which : evt.keyCode
+    if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
+        return false;
+    return true;
+}
+function Allclear() {
+    for (var j = 0; j <= 29; j++) {
+        document.getElementById('t_' + j).value = "";
+    }
+    document.getElementById('lbl_qty3').innerHTML = "0";
+    document.getElementById('lbl_amt3').innerHTML = "0";
+
+    for (var i = 0; i < 3; i++) {
+        document.getElementById('lbl_qty' + i).innerHTML = "0";
+        document.getElementById('lbl_amt' + i).innerHTML = "0";
+    }
+}
+function CheckLogin() {
+
+    alert('Login first');
+
+}
